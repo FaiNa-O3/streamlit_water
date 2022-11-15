0376a79effc51e781783ffaa8363e23ebf87e9a9
FaiNa-O3 <73303034+FaiNa-O3@users.noreply.github.com>
2022-11-16 00:20:11 +0700

    Update watering2.py

--- watering2.py
+++ watering2.py
@@ -81,6 +81,6 @@ water.input['last_watering'] = (last_water)
 
 
 water.compute()
-if st.button("watering time"):
+if st.button("CALCULATE WATERING TIME):
 
     st.write(water.output['watering_time'])
